968e3fa7c8202f4d3522aa76a1b1481b70465eb4
Alina7121 <113956800+Alina7121@users.noreply.github.com>
2024-03-29 15:38:36 -0400

    npm install

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,103 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'photo-gallery';
+  galleryInfo: any = {
+    name: 'Alina Vasilyeva',
+    studentNumber: '100799398'
+};
+currentIndex: number = 0; 
+images:any[ ]= [
+  {
+    path:"./assets/images/peonies.jpg",
+    alt:"These are peonies",
+    caption: "Peonies on black background"
+  },
+  {
+    path:"./assets/images/aster.jpg",
+    alt:"This is an aster",
+    caption: "Aster flower in bloom"
+  },
+  {
+    path:"./assets/images/chamomile.jpg",
+    alt:"This is a chamomile",
+    caption: "Chamomile flower in bloom"
+  },
+  {
+    path:"./assets/images/Chrysanthemum.jpg",
+    alt:"These are chrysanthemums",
+    caption: "Long lasting chrysanthemums"
+  },
+  {
+    path:"./assets/images/dahlia.jpg",
+    alt:"These are dahlias",
+    caption: "These are dahlias"
+  },
+  {
+    path:"./assets/images/lily.jpg",
+    alt:"These are lilies",
+    caption: "Aromatic lilies"
+  },
+  {
+    path:"./assets/images/orchid.jpg",
+    alt:"This is orchid",
+    caption: "Orchid on black background"
+  },
+  {
+    path:"./assets/images/roses.jpg",
+    alt:"These are roses",
+    caption: "Red roses for love"
+  },
+  {
+    path:"./assets/images/violet.jpg",
+    alt:"These are violets",
+    caption: "Violets are in bloom"
+  },
+  {
+    path:"./assets/images/orchid2.jpg",
+    alt:"This is orchid",
+    caption: "Orchids are blooming"
+  }
+,
+{
+  path:"./assets/images/tulips2.jpg",
+  alt:"These are tulips",
+  caption: "Colorful tulips"
+},
+{
+  path:"./assets/images/lavender.jpg",
+  alt:"Lavender field",
+  caption: "Lavender field"
+}
+
+]
+prevImage() {
+  if (this.currentIndex > 0) {
+    this.currentIndex--;
+  }
+}
+
+nextImage() {
+  if (this.currentIndex < this.images.length - 1) {
+    this.currentIndex++;
+  }
+}
+isBackDisabled(){
+ return this.currentIndex===0;
+}
+
+ 
+
+changeMainImage(index: number) {
+  this.currentIndex = index;
+}
+
+ngOnInit() {
+
+}
 }
